f69fae1f1d933270068029bdfe8e96ae12ee1945
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-08-24 17:08:19 -0300

    Delete review_python/3q.py

--- review_python/3q.py
+++
@@ -1,6 +0,0 @@
-nome = input("digite seu nome: ")
-idade = int(input("digite seu idade: "))
-if (idade < 16):
-    print(f"{nome} não está apto(a) para votar")
-else:
-    print(f"{nome} está apto(a) para votar")
