463d6458d68e2bb31eb44b8d4847591554e7effe
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-08-24 17:08:01 -0300

    Delete review_python/2q.py

--- review_python/2q.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input("digite n1: "))
-n2 = int(input("digite n2: "))
-print(f"{n1} + {n2} = {n1 + n2}")
-print(f"{n1} * {n2} = {n1 * n2}")
-print(f"{n1} - {n2} = {n1 - n2}")
-print(f"{n1} / {n2} = {n1 / n2}")
