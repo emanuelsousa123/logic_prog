9312119ba09353c6ef0ffff09924bd5cd47b4a9b
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-10-05 16:26:41 -0300

    js adicionada

---
+++ js/sentimentos/script.js
@@ -0,0 +1,11 @@
+function feliz() {
+    document.getElementById('sentimento').innerHTML='&#128512'
+}
+
+function raiva() {
+    document.getElementById('sentimento').innerHTML='&#128544'
+}
+
+function triste() {
+    document.getElementById('sentimento').innerHTML='&#128557'
+}
