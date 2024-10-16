8a336fd044f289e55f7c61cd735e04233bc28150
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-10-16 09:06:14 -0300

    enviado

---
+++ js/aula 2/script.js
@@ -0,0 +1,6 @@
+function alerta() {
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let imc = peso/(altura*altura)
+    alert(imc);
+}
