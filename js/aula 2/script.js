6cddc8a75c03e0215d7d1d7c8047f955e5c66c70
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-10-17 23:38:44 -0300

    enviado

--- js/aula 2/script.js
+++ js/aula 2/script.js
@@ -1,6 +1,16 @@
-function alerta() {
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    let imc = peso/(altura*altura)
-    alert(imc);
+
+function alerta(){
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value;
+    let imc = peso/(altura*altura);
+    document.getElementById('imc').innerHTML= imc.toFixed(1);
+    if (imc<18.5) {
+        document.getElementById('class').innerHTML= 'Abaixo do peso.';
+    } else if (18.5<imc && imc<24.9) {
+        document.getElementById('class').innerHTML= 'Peso normal.';
+    } else if (25<imc && imc<29.9) {
+        document.getElementById('class').innerHTML= 'Excesso de peso.';
+    } else {
+        document.getElementById('class').innerHTML= 'Obesidade.'
+    }
 }
